980a431203872be296bb0fb391ead459f7b1ebe0
AndrewTtofi <andreas@ttofi.com>
2022-11-09 15:11:36 +0200

    part 1 completed with unit test

--- fizzbuzz/src/main/App.java
+++ fizzbuzz/src/main/App.java
@@ -14,7 +14,16 @@ public class App {
     public HashMap<Integer,String> fizzbuzz() {
         HashMap a = new HashMap<Integer,String>();
         for (int i = 1; i < 101; i++) {
-            a.put(i, "Fizz");
+            if (i%3==0 && i%5==0){
+                a.put(i,"FizzBuzz");
+            } else if (i%3==0){
+                a.put(i, "Fizz");
+            } else if(i%5==0) {
+                a.put(i,"Buzz");
+            } else {
+                String x = String.valueOf(i);
+                a.put(i,x);
+            }
           }
         return a;
     }
--- fizzbuzz/src/test/AppTest.java
+++ fizzbuzz/src/test/AppTest.java
@@ -3,8 +3,6 @@ package test;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -34,7 +32,7 @@ public class AppTest {
         Iterate the fizzbuzz list, for each element, check if it is a multiple of 3, if it is then make sure its fizz
         */ 
         for (int i=1; i < fizzbuzz.size(); i++)
-            if (i%3==0){
+            if (i%3==0 && i%5!=0){
                 assertTrue("Fizz is not included", fizzbuzz.get(i) == "Fizz");
             }
     }
@@ -50,8 +48,20 @@ public class AppTest {
         Iterate the fizzbuzz list, for each element, check if it is a multiple of 3, if it is then make sure its fizz
         */ 
         for (int i=1; i < fizzbuzz.size(); i++)
-            if (i%3==0){
+            if (i%5==0 && i%3!=0){
                 assertTrue("Buzz is not included", fizzbuzz.get(i) == "Buzz");
+            }
+    }
+    @Test
+    public void return_fizzbuzz_for_mod_3_5() {
+        //given
+        App app = new App();
+        //when
+        HashMap<Integer,String> fizzbuzz = app.fizzbuzz();
+        //then
+        for (int i=1; i < fizzbuzz.size(); i++)
+            if (i%5==0 && i%3==0){
+                assertTrue("Buzz is not included", fizzbuzz.get(i) == "FizzBuzz");
             }
     }
 }
